2f7fb0b2860d8f554230ec49a09a01f727938449
Brian Levi <bra.levi@gmail.com>
2025-03-02 19:38:16 +0100

    feat: commit alfred api with telegram controller

---
+++ src/Babylon.Alfred/Babylon.Alfred.Api/Features/Startup/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Babylon.Alfred.Api.Features.Telegram.Extensions;
+
+namespace Babylon.Alfred.Api.Features.Startup.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void RegisterFeatures(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.RegisterTelegram();
+    }
+}
