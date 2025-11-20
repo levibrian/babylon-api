c0cec40dc8520776a2f66e9650df48c7e96322b5
Brian Levi <bra.levi@gmail.com>
2025-11-20 21:54:12 +0100

    chore: added resharper ignore

--- src/Babylon.Alfred/Babylon.Alfred.Api/Shared/Data/Models/SecurityType.cs
+++ src/Babylon.Alfred/Babylon.Alfred.Api/Shared/Data/Models/SecurityType.cs
@@ -3,10 +3,12 @@ namespace Babylon.Alfred.Api.Shared.Data.Models;
 public enum SecurityType
 {
     Stock = 1,
+    // ReSharper disable once InconsistentNaming
     ETF = 2,
     MutualFund = 3,
     Bond = 4,
     Crypto = 5,
+    // ReSharper disable once InconsistentNaming
     REIT = 6,
     Options = 7,
     Commodity = 8
